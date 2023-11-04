2d1708ca8402bf8d7fe4ff39a738beb16365b536
dzeall00 <dzuevalbert0@gmail.com>
2023-11-04 17:53:11 +0300

    Завершение программы

--- ПрограммаПитон2/ПрограммаПитон2/ПрограммаПитон2.cpp
+++ ПрограммаПитон2/ПрограммаПитон2/ПрограммаПитон2.cpp
@@ -10,6 +10,7 @@ double max(double x, double y)
     }
     return x;
 }
+
 double min(double x, double y)
 {
     if (x > y) {
@@ -29,4 +30,30 @@ bool thc(double x, double y, double z, double w, double a, double b)
     c = y - k * x;
 
     return b == a * k + c;
+}
+
+int main(int argc, char* argv[])
+{
+    setlocale(LC_ALL, "Russian");
+
+    double x, y; // Координаты начала отрезка
+    double z, w; // Координаты конца отрезка
+    double a, b; // Координаты точки
+
+    bool result;
+
+    cout << "Координаты начала отрезка: ";
+    cin >> x >> y;
+
+    cout << "Координаты конца отрезка: ";
+    cin >> z >> w;
+
+    cout << "Координаты точки: ";
+    cin >> a >> b;
+
+    result = thc(x, y, z, w, a, b);
+    cout << result << endl;
+    system("pause");
+
+    return 0;
 }
